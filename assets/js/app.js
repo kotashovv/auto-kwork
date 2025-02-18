c58b3b6ac87178cb50ca985f6cd6aee6d8fa4265
Russel <kotashov03@gmail.com>
2025-02-19 03:49:09 +0400

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,134 @@
 document.addEventListener('DOMContentLoaded', () => {
-    function EncodeText(text) {
-        let result = '';
-        let count = 1;
+    // function EncodeText(text) {
+    //     let result = '';
+    //     let count = 1;
 
-        for (i = 0; i < text.length; i++) {
-            if (text[i] === text[i + 1]) {
-                count++;
+    //     for (i = 0; i < text.length; i++) {
+    //         if (text[i] === text[i + 1]) {
+    //             count++;
+    //         } else {
+    //             if (count > 1) {
+    //                 result += count;
+    //             }
+    //             result += item[i];
+    //             count = 1;
+    //         }
+    //     }
+
+    //     return result;
+    // }
+
+    Fancybox.bind("[data-fancybox]", {
+    });
+
+
+    const burgerBtn = document.querySelector('.burger-btn')
+    const mobileMenu = document.querySelector('.mobile-header')
+
+    if (burgerBtn) {
+        burgerBtn.addEventListener('click', (e)=>{
+            if (e.target.classList.contains('active')) {
+                CloseMenu();
             } else {
-                if (count > 1) {
-                    result += count;
-                }
-                result += item[i];
-                count = 1;
+                OpenMenu();
             }
-        }
-
-        return result;
+        })
     }
+
+    function OpenMenu() {
+        document.querySelector('main.main').classList.add('filter-black')
+        burgerBtn.classList.add('active');
+        mobileMenu.classList.add('active');
+        document.body.style.overflow = 'hidden';
+        document.addEventListener('click', (e)=>{
+            if (!e.target.closest('.burger-btn') && !e.target.closest('.header')) {
+                CloseMenu();
+            }
+        })
+    }
+
+    function CloseMenu() {
+        document.querySelector('main.main').classList.remove('filter-black')
+        burgerBtn.classList.remove('active');
+        mobileMenu.classList.remove('active');
+        document.body.style.overflow = 'auto';
+    }
+
+    const faqBtns = document.querySelectorAll('.faq__btn');
+    const faqTexts = document.querySelectorAll('.faq__text');
+    if (faqBtns.length != 0) {
+        faqBtns.forEach(item => {
+            item.addEventListener('click', (e) => {
+                e.preventDefault();
+                const target = e.target;
+
+                if (!target.classList.contains('active')) {
+                    OpenFaq(target);
+                } else {
+                    CloseFaq(target);
+                }
+            })
+        })
+    }
+
+    function OpenFaq(item) {
+        faqBtns.forEach(item => {
+            item.classList.remove('active');
+        })
+        faqTexts.forEach(item => {
+            item.classList.remove('active');
+        })
+        item.classList.add('active');
+        item.nextElementSibling.classList.add('active');
+    }
+    function CloseFaq(item) {
+        faqTexts.forEach(item => {
+            item.classList.remove('active');
+        })
+        item.classList.remove('active');
+        item.nextElementSibling.classList.remove('active');
+    }
+
+
+    const popupBtns = document.querySelectorAll('.main-btn[data-popup]');
+    const popupWrap = document.querySelector('.popup');
+    const popupCloseBtns = document.querySelectorAll('.close-menu');
+
+    if (popupCloseBtns.length != 0) {
+        popupCloseBtns.forEach(item=>{
+            item.addEventListener('click', ()=>{
+                ClosePopup();
+            })
+        })
+    }
+    if (popupBtns.length != 0) {
+        popupBtns.forEach(item=>{
+            item.addEventListener('click', (e)=>{
+                e.preventDefault();
+                const target = item.getAttribute('data-popup');
+                OpenPopup(target);
+            })
+        })
+    }
+    function OpenPopup(target){
+        const targetItem = document.querySelector(target);
+
+        popupWrap.classList.add('active');
+        targetItem.classList.add('active');
+
+        document.addEventListener('click', (e)=>{
+            if (!e.target.closest('.main-btn') && !e.target.closest('.popup__item')) {
+                ClosePopup();
+            }
+        })
+    }
+
+    function ClosePopup() {
+        const popupItems = document.querySelectorAll('.popup__item')
+        popupItems.forEach(item=>{
+            item.classList.remove('active');
+        })
+        popupWrap.classList.remove('active');
+    }
+    
 })
